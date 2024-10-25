5deba8947469add66b31f6a56bb2334b30720817
Battletrout <nathanmkemp@gmail.com>
2024-10-25 10:23:47 +0000

    Add helloworld to main

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "cpp_n_gram_tokenizer/api.hpp"
+
+int main() {
+    std::cout << "Hello from C++20 project!" << std::endl;
+    
+    // Create an instance of our API class
+    cpp_n_gram_tokenizer::API api;
+    
+    return 0;
+}
